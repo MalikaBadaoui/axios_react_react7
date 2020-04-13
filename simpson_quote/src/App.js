89c8740b444f6463b1f513724e0ee09fdc746517
MalikaBadaoui <malikamyriambadaoui@gmail.com>
2020-04-13 18:10:42 +0200

    fin du projet

--- simpson_quote/src/App.js
+++ simpson_quote/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import DisplayQuotes from './components/DisplayQuotes.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const quotess = {quote:'In theory, Communism works! In theory.', 
+character:'Homer Simpson',
+image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939',
+characterDirection:'Right'};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: quotess 
+    };
+    this.fetchquotesJSON = this.fetchquotesJSON.bind(this); 
+  }
+
+  fetchquotesJSON() {   
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes') 
+      .then(response => response.data)  
+      .then(data => {       
+        this.setState({
+          quotes: data[0],   
+        });
+    });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={this.fetchquotesJSON}>Get quote</button>
+        <DisplayQuotes quotes={this.state.quotes} />
+      </div>
+    );
+  }
 }
 
 export default App;
